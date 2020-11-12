4b569526ed3791ffbdd4ce33acb9a6f92bd4813a
sebastian2161 <sebastiancoll25@gmail.com>
2020-11-11 22:42:22 -0300

    primer commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string Exec_Procesos(string var1)
+        {
+            //1) crear informacion del proceso
+            var psi = new ProcessStartInfo();
+            psi.FileName = @"C:\Users\sebastian\AppData\Local\Programs\Python\Python37\Python.exe";
+
+            //2) entregar el script py y argumentos
+            var script = @"C:\Users\sebastian\Desktop\string.py";
+            var start = var1;
+
+            psi.Arguments = $"\"{script}\" \"{start}\"";
+             
+            //3) Configuracion de procesos
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            psi.RedirectStandardOutput = true;
+            psi.RedirectStandardError = true;
+
+            //4)Executar proceso y obtener parametros de salida
+            var errors = "";
+            var results = "";
+
+            using (var process = Process.Start(psi))
+            {
+                errors = process.StandardError.ReadToEnd();
+                results = process.StandardOutput.ReadToEnd();
+
+            }
+
+            //5) Lanzar las salidas
+            Console.WriteLine("ERRORS:");
+            Console.WriteLine(errors);
+            Console.WriteLine();
+            Console.WriteLine("RESULTS:");
+            Console.WriteLine(results);
+
+            return results;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(textBox1.Text);
+
+            string resultado = Exec_Procesos(textBox1.Text);
+            MessageBox.Show(resultado);
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        //[STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            //Console.WriteLine("Executar el proceso de python 1");
+            //Exec_Procesos();
+
+
+        }
+
+        //static void Exec_Procesos()
+        //{
+        //1) crear informacion del proceso
+        //var psi = new ProcessStartInfo();
+        //psi.FileName = @"C:\Users\sebastian\AppData\Local\Programs\Python\Python37\Python.exe";
+
+        //2) entregar el script py y argumentos
+        //var script = @"C:\Users\sebastian\Desktop\string.py";
+
+        //psi.Arguments = $"\"{script}\"";
+
+        //3) Configuracion de procesos
+        //psi.UseShellExecute = false;
+        //psi.CreateNoWindow = true;
+        //psi.RedirectStandardOutput = true;
+        //psi.RedirectStandardError = true;
+
+        //4)Executar proceso y obtener parametros de salida
+        //var errors = "";
+        //var results = "";
+
+        //    using (var process = Process.Start(psi))
+        //    {
+        //errors = process.StandardError.ReadToEnd();
+        //results = process.StandardOutput.ReadToEnd();
+
+        //}
+
+        //5) Lanzar las salidas
+        //Console.WriteLine("ERRORS:");
+        //    Console.WriteLine(errors);
+        //    Console.WriteLine();
+        //    Console.WriteLine("RESULTS:");
+            //Console.WriteLine(results);
+
+
+        //}
+
+    }
+}
